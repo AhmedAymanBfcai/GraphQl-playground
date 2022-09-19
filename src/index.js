fe4c81bfb3d95b9fe88e449c7d04308546b5254f
AhmedAymanBfcai <ahmedaymanbfcai123@gmail.com>
2022-09-19 17:35:09 +0200

    challenge v2

--- src/index.js
+++ src/index.js
@@ -4,26 +4,53 @@ import { ApolloServer, gql } from "apollo-server";
 // Type definations (schema)
 const typeDefs = gql`
   type Query {
+    greeting(name: String, position: String): String! #The "!" means that you have to return an idValue and you cannot return "null" :)
+    add(a: Float!, b: Float!): Float!
     me: User!
+    post: Post!
   }
 
   type User {
-    id: ID! #The "!" means that you have to return an idValue and you cannot return "null" :)
+    id: ID!
     name: String!
     email: String!
     age: Int
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
   }
 `;
 
 // Resolvers
 const resolvers = {
   Query: {
+    greeting(parent, args, ctx, info) {
+      if (args.name && args.position) {
+        return `Hello, ${args.name}! You are my favoriate ${args.position}.`;
+      } else {
+        return "Hello!";
+      }
+    },
+    add(parent, args, ctx, info) {
+      return args.a + args.b;
+    },
     me() {
       return {
-        id: "1",
-        name: "Ahmed BFCAI",
+        id: "123098",
+        name: "Ahmed",
         email: "ahmed@example.com",
-        age: null,
+      };
+    },
+    post() {
+      return {
+        id: "092",
+        title: "GraphQL 101",
+        body: "",
+        published: false,
       };
     },
   },
